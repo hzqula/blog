3a7c853b717ccae902afc6e7e98a66af3fe4c3f9
hzqula <ilooqstrasi@gmail.com>
2025-11-28 11:32:24 +0700

    feat: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react";
 import type { Metadata } from "next";
 import { Source_Sans_3, Source_Serif_4 } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 import AuthProvider from "@/components/auth-provider";
 
@@ -11,7 +12,7 @@ const sourceSerif = Source_Serif_4({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Muhammad Faruq",
   description:
-    'Ini blog baru—yang mungkin sudah nggak baru lagi pas kalian baca—yang biasa gue anggap "Mesin Waktu". Di sini juga jadi tempat tersiksanya pikiran dan jari-jemari agar bisa menghasilkan tulisan yang—anggap aja—bermanfaat. Hehe.',
+    'Ini blog baru—yang mungkin sudah nggak baru lagi pas kalian baca—yang biasa gue anggap "Mesin Waktu". Di sini juga jadi tempat tersiksanya pikiran dan jari-jemari agar bisa ngehasilin tulisan yang—anggap aja—bermanfaat. Hehe.',
   icons: {
     icon: "/logo.svg",
   },
@@ -28,6 +29,7 @@ export default function RootLayout({
         <AuthProvider>
           {children}
           <Analytics />
+          <GoogleAnalytics gaId={process.env.GOOGLE_MEASUREMENT_ID!} />
         </AuthProvider>
       </body>
     </html>
